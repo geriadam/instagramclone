b22a4e41ab1e7ae49de79be26b38ca54a1ad1d2b
geri <geriadam1@gmail.com>
2020-05-14 01:07:29 +0700

    creating post schema and post route

---
+++ routes/post.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const Post = mongoose.model("Post")
+const requiredLogin = require('../middleware/requiredLogin')
+
+router.post('/createpost', requiredLogin, (req, res) => {
+    const { title, body } = req.body
+    if(!title && !body){
+        return res.status(442).json({error: "Please make all field fill"})
+    }
+
+    req.user.password = undefined
+    const post = new Post({
+        title,
+        body,
+        postedBy: req.user
+    })
+
+    post.save().then(result => {
+        res.json({post: result})
+    })
+    .catch(err => {
+        res.json({message: err})
+    })
+})
+
+module.exports = router
