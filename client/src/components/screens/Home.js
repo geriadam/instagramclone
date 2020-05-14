793c05cf7fbb738f14f8145711c402f0b281a1a2
geri <geriadam1@gmail.com>
2020-05-14 22:55:24 +0700

    UI Home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Geri</h5>
+                <dvi className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" />
+                </dvi>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Geri</h5>
+                <dvi className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" />
+                </dvi>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+        </div>
     )
 }
 
