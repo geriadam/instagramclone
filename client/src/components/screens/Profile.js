a3c76cbdfd1de6ede21e8ede1aa19b254e80e400
geri <geriadam1@gmail.com>
2020-05-14 22:46:50 +0700

    UI Profile Page

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div style={{margin: "0px auto", maxWidth: "550px" }}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "10px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+                <div>
+                    <img style={{width: "160px", height: "160px", borderRadius: "80px"}} src="https://geriadam.github.io/assets/images/profile.png"/>
+                </div>
+                <div>
+                    <h4>Geri Adam Saputra</h4>
+                    <div style={{display: "flex"}}>
+                        <h5>5 Post</h5>
+                        <h5>5 Follower</h5>
+                        <h5>5 Following</h5>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+            </div>
+        </div>
     )
 }
 
