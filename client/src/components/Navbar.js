530731b32fa15e72afafae725bcf1a1752dd26fc
geri <geriadam1@gmail.com>
2020-05-16 00:48:02 +0700

    changing navigation link after signin

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
+import {UserContext} from '../App'
 
 const Navbar = () => {
+    const {state, dispatch} = useContext(UserContext)
+    const renderList = () => {
+        if(state){
+            return (
+                <div>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/createpost">Create Post</Link></li>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <li><Link to="/signin">Signin</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                </div>
+            )
+        }
+    }
     return (
         <nav>
             <div className="nav-wrapper white" style={{color: "black"}}>
-                <Link to="/" className="brand-logo left">Instagram</Link>
+                <Link to={state ? "/" : "/signin"} className="brand-logo left">Instagram</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><Link to="/signin">Signin</Link></li>
-                    <li><Link to="/signup">Signup</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/createpost">Create Post</Link></li>
+                    {renderList()}
                 </ul>
             </div>
         </nav>
